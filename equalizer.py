fbb9b5cf7fff41d84c6fdbbd6928b692d88c05e9
Alexey Neustroev <alexey.neustroev@mytona.com>
2017-12-01 11:31:08 +0900

    lab3

--- equalizer.py
+++ equalizer.py
@@ -10,13 +10,31 @@ def main(filename, r):
     # и записывать результат в <filename>.equalized.png
     img = Image.open(filename)
     img.load()
-
-    a = np.array(img.getdata(), dtype=np.uint8).reshape(img.size[::-1])
-    b = np.zeros(img.size[::-1], dtype=np.uint8)
  
    # код сюда ....
 
+    a = np.array(img.getdata(),dtype=np.uint8)
+    a=a[:,0]
+    a = a.reshape(img.size[::-1])
+    b = np.zeros(img.size[::-1], dtype=np.uint8)
     
+    m, n = a.shape
+    
+    #у img.histogram() почему-то длина 512 вместо 256, пришлось собрать вручную
+    h = [0.0] * 256
+    for i in range(m):
+        for j in range(n):
+            h[a[i, j]]+=1
+    hist = np.array(h)/(m*n)
+    
+    cdf = np.cumsum(hist)
+    sk = np.uint8(255 * cdf)
+    s1, s2 = a.shape
+    
+    for i in range(0, s1):
+        for j in range(0, s2):
+            b[i, j] = sk[a[i, j]]
+
     newimg = Image.fromarray(b);
     newimg.show()
     newimg.save(filename+'.equalized.png')
